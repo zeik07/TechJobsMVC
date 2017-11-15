2ce31c9fe3f7fd78a07f617cd581ac1949846e17
Chris Bay <chris@chrisbay.net>
2017-11-15 14:46:56 -0600

    Remove old dependencies from csproj file

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using TechJobs.Models;
 
@@ -15,6 +16,25 @@ namespace TechJobs.Controllers
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
+
+        public IActionResult Results(string searchTerm, string searchType)
+        {
+            ViewBag.columns = ListController.columnChoices;
+            if (searchType == "all")
+            {
+                List<Dictionary<String, String>> jobs = JobData.FindByValue(searchTerm);
+                ViewBag.jobs = jobs;
+            }
+            else
+            {
+                List<Dictionary<String, String>> jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                ViewBag.jobs = jobs;
+            }
+            
+            ViewBag.title = "Jobs with " + searchType + ": " + searchTerm;
+            
+            return View("Views/Search/Index.cshtml");
+        }
 
     }
 }
